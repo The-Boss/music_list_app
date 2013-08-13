41782af024809a64a46e5bcf018b3f2e080c0cbb
George Bostock <george.a.bostock@gmail.com>
2013-08-13 22:17:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicListApp::Application.config.secret_token = '8ebc17980c324f9c3e0ca5d310a152efadd21466814057ccb4e33650ac89132f78aec350e83c9a7b077efb8a27396c6ffc72bef0aa2d4f53c9de4816904a2de2'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+MusicListApp::Application.routes.draw do
+
+
+end
