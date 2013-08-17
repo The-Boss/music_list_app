8a59ef28d5dec157ec6cf1688150652df2d25740
George Bostock <george.a.bostock@gmail.com>
2013-08-17 13:56:25 +0100

    got the basic login working

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,14 @@ ActiveRecord::Schema.define(:version => 20130813230933) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
-    t.boolean  "admin"
+    t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
